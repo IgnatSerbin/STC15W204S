0d84e8b2f40c3ab1a6ec151f9abef8d06dc73256
iSerbin <i.serbin@dannie.cc>
2022-10-02 20:47:22 +0300

    Added define DELAY_MS

--- src/main.c
+++ src/main.c
@@ -2,13 +2,15 @@
 #include <board.h>
 #include <delay.h>
 
+#define DELAY_MS 1000
+
 void main(){
     while (1)
     {
         LED_BUILDIN = LED_BUILDIN_ON;
-        delay_ms(1000);
+        delay_ms(DELAY_MS);
         LED_BUILDIN = LED_BUILDIN_OFF;
-        delay_ms(1000);
+        delay_ms(DELAY_MS);
     }
     
 }

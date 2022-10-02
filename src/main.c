648c7a7e66e193530dd3d3f1c5f095c7c9310281
iSerbin <i.serbin@dannie.cc>
2022-10-02 20:46:00 +0300

    Added LED flashing function

--- src/main.c
+++ src/main.c
@@ -1,5 +1,14 @@
-
+#include <STC15Fxx.h>
+#include <board.h>
+#include <delay.h>
 
 void main(){
+    while (1)
+    {
+        LED_BUILDIN = LED_BUILDIN_ON;
+        delay_ms(1000);
+        LED_BUILDIN = LED_BUILDIN_OFF;
+        delay_ms(1000);
+    }
     
 }
